8b57d5d20fedbb9ae980ebe17cf7ace52efced1b
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2020-08-30 16:28:46 -0400

    refactor(spaces):spaces format

--- app.js
+++ app.js
@@ -7,60 +7,57 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  let body = [];
-  req.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();    
-    let oBody = JSON.parse(body);
-    let uri = oBody.url
-    let uriSegment = uri.split("/"); 
-    let method = oBody.method;
-    let queryString = oBody.queryString;
+    let body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();    
+        let oBody = JSON.parse(body);
+        let uri = oBody.url;
+        let uriSegment = uri.split("/"); 
+        let method = oBody.method;
+        let queryString = oBody.queryString;
 
-    var uriTemplates = jp.query(jsonData, `$.paths`);
-    Object.keys(uriTemplates[0]).forEach(key => {
-        let uriTemplateSegment = key.split("/");
-        if (uriTemplateSegment.length === uriSegment.length) {
-            uriTemplateSegment.every(function(v, i) {
-                if (v.charAt(0) === '{' && v.charAt(v.length - 1) === '}')
-                {
-                    //TEMPLATE
-                }
-                else if (! (v === uriSegment[i]) )
-                {
-                    return false;
-                }
+        var uriTemplate = '';
+        var uriTemplates = jp.query(jsonData, `$.paths`);
+        Object.keys(uriTemplates[0]).some(key => {
+            let uriTemplateSegment = key.split("/");
+            if (uriTemplateSegment.length === uriSegment.length) {
+                uriTemplateSegment.every(function(v, i) {
+                    if (v.charAt(0) === '{' && v.charAt(v.length - 1) === '}')
+                    {
+                        //TEMPLATE
+                    }
+                    else if (! (v === uriSegment[i]) )
+                        return false;
 
-                if (i === uriTemplateSegment.length -1 )
-                {
-                    uriTemplate = key;
-                    return false;
-                }
+                    if (i === uriTemplateSegment.length -1 )
+                        uriTemplate = key;
+                    return true;
+                });
+            }
+            if (uriTemplate) 
                 return true;
-            });
-        }
-        //if (uriTemplate) break;
-    });console.log(`uriTemplate: ${uriTemplate}`);
+        });console.log(`uriTemplate: ${uriTemplate}`);
 
-    var parameters = jp.query(jsonData, `$.paths["${uriTemplate}"]..${method}..parameters.*`);
-    let oQueryString = qs.parse(queryString);
-    Object.keys(oQueryString).forEach(function(key) {console.log(key);
-        if ( parameters.find(o => o.name === key && o.in == "query") ) {
-            console.log(`exist ${key}`);
-        }
-        else
-        {
-            console.log(`not exist ${key}`);
-        }
-      });
-  });
+        var parameters = jp.query(jsonData, `$.paths["${uriTemplate}"]..${method}..parameters.*`);
+        let oQueryString = qs.parse(queryString);
+        Object.keys(oQueryString).forEach(function(key) {console.log(key);
+            if ( parameters.find(o => o.name === key && o.in == "query") ) {
+                console.log(`exist ${key}`);
+            }
+            else
+            {
+                console.log(`not exist ${key}`);
+            }
+        });
+    });
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello World`);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
