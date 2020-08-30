6cf2ffc5699ce554f53d3a1653e3dcecf2327b58
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2020-08-30 15:10:41 -0400

    refactor(comments):delete commnets

--- app.js
+++ app.js
@@ -6,55 +6,41 @@ const jsonData = require('./openapi.json');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const cities = [
-    { name: "London", "population": 8615246 },
-    { name: "Berlin", "population": 3517424 },
-    { name: "Madrid", "population": 3165235 },
-    { name: "Rome",   "population": 2870528 }
-  ];
-
 const server = http.createServer((req, res) => {
-  const { method, url } = req;
-//   console.log(method);
-//   console.log(url);
-
   let body = [];
   req.on('data', (chunk) => {
     body.push(chunk);
   }).on('end', () => {
-    body = Buffer.concat(body).toString();//console.log(body);
-    
+    body = Buffer.concat(body).toString();    
     let oBody = JSON.parse(body);
-    let uri = oBody.url;console.log(uri);let aUri = uri.split("/"); 
-    let method = oBody.method;console.log(method);
-    let queryString = oBody.queryString;console.log(queryString);
+    let uri = oBody.url
+    let uriSegment = uri.split("/"); 
+    let method = oBody.method;
+    let queryString = oBody.queryString;
 
-    var uriTemplates = jp.query(jsonData, `$.paths`);//console.log(uriTemplates);
-    Object.keys(uriTemplates[0]).forEach(key => {console.log(`key: ${key}`);console.log(`uri: ${uri}`);
-        aUriTemplate = key.split("/");
-        if (aUriTemplate.length === aUri.length) {
-            aUriTemplate.every(function(v, i) {console.log(`v: ${v} aUri[i]: ${aUri[i]} compare: ${v === aUri[i]}`)
+    var uriTemplates = jp.query(jsonData, `$.paths`);
+    Object.keys(uriTemplates[0]).forEach(key => {
+        let uriTemplateSegment = key.split("/");
+        if (uriTemplateSegment.length === uriSegment.length) {
+            uriTemplateSegment.every(function(v, i) {
                 if (v.charAt(0) === '{' && v.charAt(v.length - 1) === '}')
                 {
-                    console.log(`TEMPLATE: ${v}`)
                     //TEMPLATE
                 }
-                else if (! (v === aUri[i]) )
+                else if (! (v === uriSegment[i]) )
                 {
-                    console.log(`return`)
                     return false;
                 }
 
-                if (i === aUriTemplate.length -1 )
+                if (i === uriTemplateSegment.length -1 )
                 {
-                    uriTemplate = key;console.log(uriTemplate);
+                    uriTemplate = key;
                     return false;
                 }
                 return true;
             });
         }
         //if (uriTemplate) break;
-        //aUriTemplates.push(key);//console.log(key);
     });console.log(`uriTemplate: ${uriTemplate}`);
 
     var parameters = jp.query(jsonData, `$.paths["${uriTemplate}"]..${method}..parameters.*`);
